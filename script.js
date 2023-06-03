c4a51f848f7b17de6fc45caba709d4e82b038506
Félix Laviéville <lavieville.felix@gmail.com>
2023-06-03 12:04:19 +0200

    lmise a jour

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-
-let motSecret = undefined
+let motSecret = []
 let indice = undefined
 
 fetch('https://trouve-mot.fr/api/random').then(r => r.json()).then(d => {
-    motSecret = d[0].name
+    // On isole toutes les lettres du mot à deviner
+    const mesLettres = d[0].name.split("")
+    // Pour chaque lettre, on va transformer "a" en structure : {lettre: "a", trouvee: false}
+    // on utilise la méthode "map"
+    motSecret = mesLettres.map((lettre) => {
+        return { lettre: lettre, trouvee: false }
+    })
+
+    console.log(motSecret)
+
+    // on affiche l'indice dans le DOM
     indice = d[0].categorie
     document.querySelector('.indice').innerHTML = indice
+
 })
