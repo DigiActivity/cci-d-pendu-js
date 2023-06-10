25e0fd14b46f3ea331dd6635438753d3b8b35683
Félix Laviéville <lavieville.felix@gmail.com>
2023-06-10 11:15:40 +0200

    formulaire pour récupérer la lettre

--- script.js
+++ script.js
@@ -2,18 +2,27 @@ let motSecret = []
 let indice = undefined
 
 fetch('https://trouve-mot.fr/api/random').then(r => r.json()).then(d => {
-    // On isole toutes les lettres du mot à deviner
+
     const mesLettres = d[0].name.split("")
-    // Pour chaque lettre, on va transformer "a" en structure : {lettre: "a", trouvee: false}
-    // on utilise la méthode "map"
+
     motSecret = mesLettres.map((lettre) => {
         return { lettre: lettre, trouvee: false }
     })
 
     console.log(motSecret)
 
-    // on affiche l'indice dans le DOM
     indice = d[0].categorie
     document.querySelector('.indice').innerHTML = indice
 
+    const formulaireLettre = document.querySelector('#formulaire-lettre')
+
+    formulaireLettre.addEventListener('submit', (e) => {
+        // empêcher le rechargement de la page
+        e.preventDefault()
+        console.log("test")
+        // récupérer les données du formulaire
+        const data = new FormData(e.target)
+        const lettre = data.get("lettre")
+        console.log("Ma lettre :", lettre)
+    })
 })
